b874b5931037e487ac18fa43ae5ceb5048854913
Bryan Wolfford <bryan@wolfford.com>
2018-06-22 13:58:00 +0200

    added CUDA tutorial

---
+++ CUDA Tutorial/add_grid.cu
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+
+// add elements of x and y, store into y
+__global__
+void add(int n, float *x, float *y){
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	int stride = blockDim.x * gridDim.x;
+	for(int i = index; i < n; i += stride)
+		y[i] = x[i] + y[i];
+}
+
+int main(void){
+	int N = 1<<20; // 1 million elements
+	
+	float *x, *y;
+	cudaMallocManaged(&x, N*sizeof(float));
+	cudaMallocManaged(&y, N*sizeof(float));
+	
+	//initialize x and y arrays on the host
+	for(int i = 0; i < N; i++){
+		x[i] = 1.0f;
+		y[i] = 2.0f;
+	}
+	
+	//run the kernel on 1M elements on the CPU
+	int blockSize = 256;
+	int numBlocks = (N + blockSize - 1) / blockSize;
+	add<<<numBlocks, blockSize>>>(N, x, y);
+	
+	//wait for GPU to finish before accessing on host
+	cudaDeviceSynchronize();
+	
+	//check for errors (all values should be 3.0f)
+	float maxError = 0.0f;
+	for(int i = 0; i < N; i++)
+		maxError = fmax(maxError, fabs(y[i]-3.0f));
+	std::cout << "Max error: " << maxError << std::endl;
+	
+	//free memory
+	cudaFree(x);
+	cudaFree(y);
+	
+	return 0;
+}
