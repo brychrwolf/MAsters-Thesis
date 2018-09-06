d58ddb6fa6405cdf678a4e451a748f0fbcb8c2fd
Bryan Wolfford <bryan.wolfford@iwr.uni-heidelberg.de>
2018-09-06 13:44:20 +0200

    enhanced CudaAccess, added props, calculates warp and block sizes

--- code/cudaAccess.cu
+++ code/cudaAccess.cu
@@ -1,59 +1,156 @@
 #include <stdio.h>
+
+#include <vector>
+#include <string>
+
 #include "cudaAccess.cuh"
 
 CudaAccess::CudaAccess(){
-	//TODO: Maintain properties for each device
-	updateDeviceCount();
+	updateDeviceProperties();
 }
 
-int CudaAccess::getDeviceCount(){
-	printf("deviceCount %d\n", deviceCount);
-	return deviceCount;
-}
-
-int CudaAccess::getIdealBlockSizeForProblemOfSize(int n){
-	//TODO: research required calculations for optimization
-	if(n > (1024 * 1024))		// 1,048,576
-		return 1024;
-	else if(n > (512 * 512))	//   262,144
-		return 512;
-	else if(n > (256 * 256))	//    65,536
-		return 256;
-	else if(n > (128 * 128))	//    16,384
-		return 128;
-	else if(n > (64 * 64))		//     4,096
-		return 64;
-	else
-		return 32;
-}
-
-
-
-void CudaAccess::updateDeviceCount(){
+void CudaAccess::updateDeviceProperties(){
 	cudaError_t err = cudaGetDeviceCount(&deviceCount);
 	if(err != cudaSuccess)
 		deviceCount = 0;
+	for(int d = 0; d < deviceCount; d++){
+		cudaDeviceProp devProp;
+		cudaGetDeviceProperties(&devProp, d);
+		deviceProperties.push_back(devProp);
+		
+		if(devProp.warpSize < minWarpSize)
+			minWarpSize = devProp.warpSize;
+		if(devProp.maxThreadsPerBlock < minMaxBlockSize)
+			minMaxBlockSize = devProp.maxThreadsPerBlock;
+	}
+}
+
+
+
+int CudaAccess::getDeviceCount(){
+	return deviceCount;
+}
+
+std::string CudaAccess::getDeviceName(int d){
+	return deviceProperties[d].name;
+}
+
+int CudaAccess::getPciBusID(int d){
+	return deviceProperties[d].pciBusID;
+}
+
+int CudaAccess::getPciDeviceID(int d){
+	return deviceProperties[d].pciDeviceID;
+}
+
+int CudaAccess::getPciDomainID(int d){
+	return deviceProperties[d].pciDomainID;
+}
+
+int CudaAccess::getMajor(int d){
+	return deviceProperties[d].major;
+}
+
+int CudaAccess::getMinor(int d){
+	return deviceProperties[d].minor;
+}
+
+int CudaAccess::getMultiProcessorCount(int d){
+	return deviceProperties[d].multiProcessorCount;
+}
+
+int CudaAccess::getClockRate(int d){
+	return deviceProperties[d].clockRate;
+}
+
+int CudaAccess::getMemoryClockRate(int d){
+	return deviceProperties[d].memoryClockRate;
+}
+
+int CudaAccess::getL2CacheSize(int d){
+	return deviceProperties[d].l2CacheSize;
+}
+
+size_t CudaAccess::getSharedMemPerMultiprocessor(int d){
+	return deviceProperties[d].sharedMemPerMultiprocessor;
+}
+
+size_t CudaAccess::getTotalConstMem(int d){
+	return deviceProperties[d].totalConstMem;
+}
+
+int CudaAccess::getManagedMemory(int d){
+	return deviceProperties[d].managedMemory;
+}
+
+int CudaAccess::getWarpSize(int d){
+	return deviceProperties[d].warpSize;
+}
+
+int CudaAccess::getMaxThreadsPerBlock(int d){
+	return deviceProperties[d].maxThreadsPerBlock;
+}
+
+int CudaAccess::getMaxThreadsPerMultiProcessor(int d){
+	return deviceProperties[d].maxThreadsPerMultiProcessor;
+}
+
+int* CudaAccess::getMaxThreadsDim(int d){
+	return deviceProperties[d].maxThreadsDim;
+}
+
+int* CudaAccess::getMaxGridSize(int d){
+	return deviceProperties[d].maxGridSize;
+}
+
+
+
+int CudaAccess::getMinWarpSize(){
+	return minWarpSize;
+}
+
+int CudaAccess::getMinMaxBlockSize(){
+	return minMaxBlockSize;
+}
+
+int CudaAccess::getIdealBlockSizeForProblemOfSize(int n){
+	if(n > (32 * 1024) && minMaxBlockSize >= 1024)		// 32,768
+		return 1024;
+	else if(n > (32 * 512) && minMaxBlockSize >= 512)	// 16,384
+		return 512;
+	else if(n > (32 * 256) && minMaxBlockSize >= 256)	//  8,192
+		return 256;
+	else if(n > (32 * 128) && minMaxBlockSize >= 128)	//  4,096
+		return 128;
+	else if(n > (32 * 64) && minMaxBlockSize >= 64)		//  2,048
+		return 64;
+	else
+		return minWarpSize;								// very likely 32
 }
 
 
 
 void CudaAccess::printCUDAProps(){
 	printf("There are %d CUDA devices.\n", deviceCount);
-
-	// Iterate through devices
-	for (int i = 0; i < deviceCount; ++i)
-	{
-		// Get device properties
-		printf("\nCUDA Device #%d\n", i);
-		cudaDeviceProp devProp;
-		cudaGetDeviceProperties(&devProp, i);
-
-    	printf("Name:                          %s\n",  devProp.name);
-    	printf("Number of multiprocessors:     %d\n",  devProp.multiProcessorCount);
-    	printf("Clock rate:                    %d\n",  devProp.clockRate);
-    	printf("Total constant memory:         %u\n",  devProp.totalConstMem);
-		printf("CUDA Capability Major/Minor version number:    %d.%d\n", devProp.major, devProp.minor);
-
+	for (int d = 0; d < deviceCount; d++){
+		printf("\nCUDA Device #%d\n", d);
+		printf("\tName:                               %s\n",  deviceProperties[d].name);
+		printf("\tPCI Bus.Device.Domain Ids:          %d.%d.%d\n",  deviceProperties[d].pciBusID,  deviceProperties[d].pciDeviceID,  deviceProperties[d].pciDomainID);
+		printf("\tCUDA Capability Major/Minor version:%d.%d\n", deviceProperties[d].major, deviceProperties[d].minor);
+		printf("\tNumber of multiprocessors:          %d\n",  deviceProperties[d].multiProcessorCount);
+		printf("\tClock rate (kHz):                   %d\n",  deviceProperties[d].clockRate);
+		printf("\tmemoryClockRate (kHz):              %d\n",  deviceProperties[d].memoryClockRate);
+		printf("\tl2CacheSize (bytes):                %d\n",  deviceProperties[d].l2CacheSize);
+		printf("\tsharedMemPerMultiprocessor (bytes): %d\n",  deviceProperties[d].sharedMemPerMultiprocessor); //size_t
+		printf("\ttotalConstMem (bytes):              %d\n",  deviceProperties[d].totalConstMem); //size_t
+		printf("\tmanagedMemory (T/F):                %d\n",  deviceProperties[d].managedMemory);
+		printf("\twarpSize (threads):                 %d\n",  deviceProperties[d].warpSize);
+		printf("\tmaxThreadsPerBlock:                 %d\n",  deviceProperties[d].maxThreadsPerBlock);
+		printf("\tmaxThreadsPerMultiProcessor:        %d\n",  deviceProperties[d].maxThreadsPerMultiProcessor);
+		printf("\tmaxThreadsDim[3]:                  [%d, %d, %d]\n",  deviceProperties[d].maxThreadsDim[0],  deviceProperties[d].maxThreadsDim[1],  deviceProperties[d].maxThreadsDim[2]);
+		printf("\tmaxGridSize[3]:                    [%d, %d, %d]\n",  deviceProperties[d].maxGridSize[0],  deviceProperties[d].maxGridSize[1],  deviceProperties[d].maxGridSize[2]);
 	}
+	printf("\nMinimum Warp Size in system:        %d\n",  minWarpSize);
+	printf("Minimum Max Block Size in system:   %d\n",  minMaxBlockSize);
 }
 
--- code/cudaAccess.cuh
+++ code/cudaAccess.cuh
@@ -1,22 +1,43 @@
 #ifndef CUDAACCESS_H
 #define CUDAACCESS_H
 
+#include <vector>
+#include <string>
 
-
-class CudaAccess{
-		const int WARP_SIZE = 32;
-		const int MAX_BLOCK_SIZE_LT2 = 512;
-		const int MAX_BLOCK_SIZE_GT2 = 1024;
-		
+class CudaAccess{		
 		int deviceCount;
+		std::vector<cudaDeviceProp> deviceProperties;
+		int minWarpSize = 32;
+		int minMaxBlockSize = 1024;
 
 	public:
 		CudaAccess();
 
-		int getDeviceCount();
-		int getIdealBlockSizeForProblemOfSize(int n);
+		void updateDeviceProperties();
 
-		void updateDeviceCount();
+		int getDeviceCount();
+		std::string getDeviceName(int d);
+		int getPciBusID(int d);
+		int getPciDeviceID(int d);
+		int getPciDomainID(int d);
+		int getMajor(int d);
+		int getMinor(int d);
+		int getMultiProcessorCount(int d);
+		int getClockRate(int d);
+		int getMemoryClockRate(int d);
+		int getL2CacheSize(int d);
+		size_t getSharedMemPerMultiprocessor(int d);
+		size_t getTotalConstMem(int d);
+		int getManagedMemory(int d);
+		int getWarpSize(int d);
+		int getMaxThreadsPerBlock(int d);
+		int getMaxThreadsPerMultiProcessor(int d);
+		int* getMaxThreadsDim(int d);
+		int* getMaxGridSize(int d);
+		
+		int getMinWarpSize();
+		int getMinMaxBlockSize();
+		int getIdealBlockSizeForProblemOfSize(int n);
 
 		void printCUDAProps();
 };
